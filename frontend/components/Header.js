1769c8e72418ddb36253b471a869c422f200ee48
maxwell3739 <maxwell@uvic.ca>
2017-07-14 23:43:19 -0700

    minor changes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomeHeaderComponent = () => {
     return (
       <div className = "header pink">
-        <img src="/img/tutorme.png" className = "logo"/>
+        <Link to="/">
+          <img src="/img/tutorme.png" className = "logo"/>
+        </Link>
       </div>
     );
 };
--- frontend/components/Home/HomeComponent.js
+++ frontend/components/Home/HomeComponent.js
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import HomeHeaderComponent from '../Header';
 import HomeTopComponent from './Top';
 import HomeBottomComponent from '../Login/SplashBottom';
 
 const HomeComponent = () => {
     return (
         <div>
-            <HomeHeaderComponent/>
             <HomeTopComponent/>
             <HomeBottomComponent/>
         </div>
